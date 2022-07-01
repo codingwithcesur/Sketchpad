f6d69a1c8e3af9e5cacc1fef6eb58ee54d034e41
abkrew <silvernoobxd@gmail.com>
2022-07-01 23:27:18 +0300

    add changing color when hovering

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 // Add function that takes input, creates ${input} number of divs inside the container.
 // All the divs should share same space filling the container fully.
+function hover(event) {
+  event.target.style.backgroundColor = "blue";
+
+  setTimeout(function () {
+    event.target.style.backgroundColor = "";
+  }, 150);
+}
+// function colorPicker(event) {
+//   event.classList.add("grid-blue");
+// }
+// div.addEventListener("click"), colorPicker;
 function addDivs(divNumber) {
   const container = document.querySelector(".container");
   let styleSheet = document.styleSheets[0];
-
   let divNeeded = divNumber * divNumber;
   let sizeCalc = `${650 / divNumber}`;
-  let grid = document.getElementsByClassName("grid");
+
   styleSheet.insertRule(`.grid {height:${sizeCalc}px;}`, 0);
   styleSheet.insertRule(`.grid {width:${sizeCalc}px;}`, 0);
+
   for (let i = 0; i < divNeeded; i++) {
     let div = document.createElement("div");
     container.appendChild(div);
     div.classList.add("grid");
-    div.textContent = "a";
+    div.addEventListener("mouseover", hover);
   }
 }
 addDivs(40);
