c49d5c9f8afa4bc5580e1fa2afd4d3d4861912dc
abkrew <silvernoobxd@gmail.com>
2022-07-02 18:54:42 +0300

    add black and white color changing buttons

--- script.js
+++ script.js
@@ -5,26 +5,40 @@
 //     event.target.style.backgroundColor = "blue";
 //   }, 50);
 // }
+const whiteBtn = document.querySelector(".white-btn");
+const blackBtn = document.querySelector(".black-btn");
+const resetBtn = document.querySelector(".reset-btn");
+let currentColor;
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+whiteBtn.addEventListener("click", () => {
+  currentColor = "white";
+  addDivs(50);
+});
+blackBtn.addEventListener("click", () => {
+  currentColor = "black";
+  addDivs(50);
+});
+// resetBtn.addEventListener("click", () => {
+//   let divAll = document.querySelectorAll("div");
+//   divAll.remove();
+//   addDivs(50);
+// });
 function colorPicker(event) {
   if (event.type === "mouseover" && !mouseDown) {
     return;
   } else {
-    event.target.style.backgroundColor = "green";
+    event.target.style.backgroundColor = currentColor;
   }
 }
 function addDivs(divNumber) {
-  const container = document.querySelector(".container");
-
+  const container = document.querySelector(".grid-container");
   let styleSheet = document.styleSheets[0];
   let divNeeded = divNumber * divNumber;
   let sizeCalc = `${650 / divNumber}`;
-
   styleSheet.insertRule(`.grid {height:${sizeCalc}px;}`, 0);
   styleSheet.insertRule(`.grid {width:${sizeCalc}px;}`, 0);
-
   for (let i = 0; i < divNeeded; i++) {
     let div = document.createElement("div");
     div.classList.add("grid");
@@ -34,4 +48,3 @@ function addDivs(divNumber) {
     container.appendChild(div);
   }
 }
-addDivs(50);
