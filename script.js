c58a78de404fc006f7d4a56b0775f05dd3ce84b4
abkrew <silvernoobxd@gmail.com>
2022-07-03 12:44:15 +0300

    fix changing size bug

--- script.js
+++ script.js
@@ -5,21 +5,29 @@
 //     event.target.style.backgroundColor = "blue";
 //   }, 50);
 // }
+let styleSheet = document.styleSheets[0];
 let container = document.querySelector(".grid-container");
 const whiteBtn = document.querySelector(".white-btn");
 const blackBtn = document.querySelector(".black-btn");
 const resetBtn = document.querySelector(".reset-btn");
 const gridSizeBtn = document.querySelector(".grid-size-btn");
-let nodesToRemove = container.getElementsByClassName("grid");
+let nodesToRemove = document.getElementsByClassName("grid");
 let currentColor;
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+container.onmousedown = () => (mouseDown = true);
+container.onmouseup = () => (mouseDown = false);
 
 function generateInput() {
   let getInputSize = document.querySelector(".grid-size-input").value;
   let inputText = document.querySelector(".input-text");
   if (getInputSize >= 2 && getInputSize <= 100) {
+    if (nodesToRemove.length > getInputSize * 1) {
+      for (let i = nodesToRemove.length - 1; i >= 0; i--) {
+        let singleNode = nodesToRemove[i];
+        // singleNode.classList.remove("grid");
+        container.removeChild(singleNode);
+      }
+    }
     inputText.textContent = "";
     addDivs(getInputSize);
   } else {
@@ -29,26 +37,21 @@ function generateInput() {
   }
 }
 function clearNodes() {
-  // if (nodesToRemove > getInputSize) {
-  //   for (let i = nodesToRemove.length - 1; i >= 0; i--) {
-  //     let singleNode = nodesToRemove[i];
-  //     container.removeChild(singleNode);
-  //   }
-  // } else {
   for (let i = nodesToRemove.length - 1; i >= 0; i--) {
     let singleNode = nodesToRemove[i];
     singleNode.style.backgroundColor = "white";
-    // }
   }
 }
 function addDivs(divNumber) {
-  let styleSheet = document.styleSheets[0];
   let divNeeded = divNumber * divNumber;
   let sizeCalc = `${650 / divNumber}`;
-  styleSheet.insertRule(`.grid {height:${sizeCalc}px;}`, 0);
-  styleSheet.insertRule(`.grid {width:${sizeCalc}px;}`, 0);
+
+  // styleSheet.insertRule(`.grid {height:${sizeCalc}px;}`, 0);
+  // styleSheet.insertRule(`.grid {width:${sizeCalc}px;}`, 0);
   for (let i = 0; i < divNeeded; i++) {
     let div = document.createElement("div");
+    div.style.width = `${sizeCalc}px`;
+    div.style.height = `${sizeCalc}px`;
     div.classList.add("grid");
     // div.addEventListener("mouseover", hover);
     div.addEventListener("mouseover", colorPicker);
